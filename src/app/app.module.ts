78e22519b71c95c6b4af598dd7e38a1a8f49d26f
Benjamin Hartwig <b.hartwig@hm.edu>
2019-12-10 12:11:28 +0100

    Added routing skeleton, created links.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  // { path: 'Erinnerungen', component: ReminderComponent },
+  // { path: 'Checklisten', component: ChecklistComponent },
+  // { path: 'Notfallkontakte', component: EmergencyContactsComponent },
+  { path: 'MainMenu', component: MainMenuComponent},
+  { path: '', redirectTo: 'MainMenu', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
