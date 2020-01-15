a4332e135871433123b1dc5e4855a38fb49d421b
HyCraftHD <hycrafthd@live.de>
2020-01-15 11:24:23 +0100

    Add basic create form

--- src/app/components/reminder/erstellen/erstellen.component.ts
+++ src/app/components/reminder/erstellen/erstellen.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ReminderService } from 'src/app/services/reminder.service';
+import { Router } from '@angular/router';
+import { Reminder } from 'src/app/classes/reminder';
 
 @Component({
   selector: 'app-erstellen',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErstellenComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  description: string;
+  date: Date;
+  location: string;
+
+  constructor(public reminderService : ReminderService, private router : Router) { }
+
+  create() {
+    console.log(this.title);
+    const reminder = new Reminder();
+    reminder.title = this.title;
+    reminder.description = this.description;
+    reminder.date = this.date;
+    reminder.location = this.location;
+    this.reminderService.addOrUpdateReminder(reminder);
+    this.router.navigate(['/', 'Erinnerungen']);
+  }
 
   ngOnInit() {
   }
